b8399e9b17d73217e80de45c4c7e69f7127797e1
sairam <sairam2772005@gmail.com>
2025-11-21 14:42:16 +0530

    file added

---
+++ sample.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ 	int a,b,c=0;
+ 	printf("Enter a:\n");
+ 	scanf("%d",&a);
+ 	printf("Enter b:\n");
+ 	scanf("%d",&b);
+ 	c=a+b;
+ 	printf("sum c= %d\n",c);
+	
+	return 0;
+}
